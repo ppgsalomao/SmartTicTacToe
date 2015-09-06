c81bdc6828e7256c503ebaa834ea23b115be8a3c
pedro.salomao <ppgsalomao@gmail.com>
2015-09-06 17:25:37 -0300

    Adding ButterKnife library. Improving Game Configuration Activity code.

--- Interface/src/main/java/br/com/salomao/smarttictactoe/view/GameConfigurationActivity.java
+++ Interface/src/main/java/br/com/salomao/smarttictactoe/view/GameConfigurationActivity.java
@@ -2,7 +2,6 @@ package br.com.salomao.smarttictactoe.view;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 
 import br.com.salomao.smarttictactoe.R;
@@ -10,17 +9,26 @@ import br.com.salomao.smarttictactoe.model.GameConfiguration;
 import br.com.salomao.smarttictactoe.model.GameLevelEnum;
 import br.com.salomao.smarttictactoe.model.GameStarterEnum;
 import br.com.salomao.smarttictactoe.model.SymbolEnum;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class GameConfigurationActivity extends AppCompatActivity {
 
-    private Button levelEasyButton;
-    private Button levelMediumButton;
-    private Button levelHardButton;
-    private Button symbolXButton;
-    private Button symbolOButton;
-    private Button gameStarterUserButton;
-    private Button gameStarterComputerButton;
-    private Button playButton;
+    @Bind(R.id.game_configuration_level_easy_button)
+    Button levelEasyButton;
+    @Bind(R.id.game_configuration_level_medium_button)
+    Button levelMediumButton;
+    @Bind(R.id.game_configuration_level_hard_button)
+    Button levelHardButton;
+    @Bind(R.id.game_configuration_user_symbol_x_button)
+    Button symbolXButton;
+    @Bind(R.id.game_configuration_user_symbol_o_button)
+    Button symbolOButton;
+    @Bind(R.id.game_configuration_game_starter_user_button)
+    Button gameStarterUserButton;
+    @Bind(R.id.game_configuration_game_starter_computer_button)
+    Button gameStarterComputerButton;
 
     private GameConfiguration gameConfiguration;
 
@@ -28,6 +36,7 @@ public class GameConfigurationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_configuration);
+        ButterKnife.bind(this);
     }
 
     @Override
@@ -39,100 +48,33 @@ public class GameConfigurationActivity extends AppCompatActivity {
     private void resetScreenStateToDefault() {
         this.gameConfiguration = new GameConfiguration();
 
-        // Game Level
-
         this.chooseLevel(this.gameConfiguration.getGameLevel());
-
-        this.levelEasyButton().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GameConfigurationActivity.this.chooseLevel(GameLevelEnum.EASY);
-            }
-        });
-
-        this.levelMediumButton().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GameConfigurationActivity.this.chooseLevel(GameLevelEnum.MEDIUM);
-            }
-        });
-
-        this.levelHardButton().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GameConfigurationActivity.this.chooseLevel(GameLevelEnum.HARD);
-            }
-        });
-
-        // User Symbol
-
         this.chooseUserSymbol(this.gameConfiguration.getUserSymbol());
-
-        this.symbolXButton().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GameConfigurationActivity.this.chooseUserSymbol(SymbolEnum.X);
-            }
-        });
-
-        this.symbolOButton().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GameConfigurationActivity.this.chooseUserSymbol(SymbolEnum.O);
-            }
-        });
-
-        // Game Starter
-
         this.chooseGameStarter(this.gameConfiguration.getGameStarter());
-
-        this.gameStarterUserButton().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GameConfigurationActivity.this.chooseGameStarter(GameStarterEnum.USER);
-            }
-        });
-
-        this.gameStarterComputerButton().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GameConfigurationActivity.this.chooseGameStarter(GameStarterEnum.COMPUTER);
-            }
-        });
-
-        // Play
-
-        this.playButton().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GameActivity.startActivity(GameConfigurationActivity.this,
-                        GameConfigurationActivity.this.gameConfiguration);
-            }
-        });
     }
 
     private void chooseLevel(GameLevelEnum gameLevel) {
         this.gameConfiguration.setGameLevel(gameLevel);
 
         // Reset buttons state
-        this.resetLevelButtonDefault(this.levelEasyButton());
-        this.resetLevelButtonDefault(this.levelMediumButton());
-        this.resetLevelButtonDefault(this.levelHardButton());
+        this.resetLevelButtonDefault(this.levelEasyButton);
+        this.resetLevelButtonDefault(this.levelMediumButton);
+        this.resetLevelButtonDefault(this.levelHardButton);
 
         // Set the color for the selected button
         switch (gameLevel) {
             case MEDIUM:
-                this.levelMediumButton().setBackgroundColor(this.getResources().getColor(R.color.light_yellow));
-                this.levelMediumButton().setTextColor(this.getResources().getColor(R.color.dark_yellow));
+                this.levelMediumButton.setBackgroundColor(this.getResources().getColor(R.color.light_yellow));
+                this.levelMediumButton.setTextColor(this.getResources().getColor(R.color.dark_yellow));
                 break;
             case HARD:
-                this.levelHardButton().setBackgroundColor(this.getResources().getColor(R.color.light_red));
-                this.levelHardButton().setTextColor(this.getResources().getColor(R.color.dark_red));
+                this.levelHardButton.setBackgroundColor(this.getResources().getColor(R.color.light_red));
+                this.levelHardButton.setTextColor(this.getResources().getColor(R.color.dark_red));
                 break;
             case EASY:
             default:
-                this.levelEasyButton().setBackgroundColor(this.getResources().getColor(R.color.light_green));
-                this.levelEasyButton().setTextColor(this.getResources().getColor(R.color.dark_green));
+                this.levelEasyButton.setBackgroundColor(this.getResources().getColor(R.color.light_green));
+                this.levelEasyButton.setTextColor(this.getResources().getColor(R.color.dark_green));
         }
     }
 
@@ -145,19 +87,19 @@ public class GameConfigurationActivity extends AppCompatActivity {
         this.gameConfiguration.setUserSymbol(symbol);
 
         // Reset buttons state
-        this.resetSymbolButtonDefault(this.symbolXButton());
-        this.resetSymbolButtonDefault(this.symbolOButton());
+        this.resetSymbolButtonDefault(this.symbolXButton);
+        this.resetSymbolButtonDefault(this.symbolOButton);
 
         // Set the color for the selected button
         switch (symbol) {
             case O:
-                this.symbolOButton().setBackgroundColor(this.getResources().getColor(R.color.light_red));
-                this.symbolOButton().setTextColor(this.getResources().getColor(R.color.dark_red));
+                this.symbolOButton.setBackgroundColor(this.getResources().getColor(R.color.light_red));
+                this.symbolOButton.setTextColor(this.getResources().getColor(R.color.dark_red));
                 break;
             case X:
             default:
-                this.symbolXButton().setBackgroundColor(this.getResources().getColor(R.color.light_blue));
-                this.symbolXButton().setTextColor(this.getResources().getColor(R.color.dark_blue));
+                this.symbolXButton.setBackgroundColor(this.getResources().getColor(R.color.light_blue));
+                this.symbolXButton.setTextColor(this.getResources().getColor(R.color.dark_blue));
         }
     }
 
@@ -170,19 +112,19 @@ public class GameConfigurationActivity extends AppCompatActivity {
         this.gameConfiguration.setGameStarter(gameStarter);
 
         // Reset buttons state
-        this.resetGameStarterButtonDefault(this.gameStarterUserButton());
-        this.resetGameStarterButtonDefault(this.gameStarterComputerButton());
+        this.resetGameStarterButtonDefault(this.gameStarterUserButton);
+        this.resetGameStarterButtonDefault(this.gameStarterComputerButton);
 
         // Set the color for the selected button
         switch (this.gameConfiguration.getGameStarter()) {
             case COMPUTER:
-                this.gameStarterComputerButton().setBackgroundColor(this.getResources().getColor(R.color.light_blue_gray));
-                this.gameStarterComputerButton().setTextColor(this.getResources().getColor(R.color.dark_blue_gray));
+                this.gameStarterComputerButton.setBackgroundColor(this.getResources().getColor(R.color.light_blue_gray));
+                this.gameStarterComputerButton.setTextColor(this.getResources().getColor(R.color.dark_blue_gray));
                 break;
             case USER:
             default:
-                this.gameStarterUserButton().setBackgroundColor(this.getResources().getColor(R.color.light_blue_gray));
-                this.gameStarterUserButton().setTextColor(this.getResources().getColor(R.color.dark_blue_gray));
+                this.gameStarterUserButton.setBackgroundColor(this.getResources().getColor(R.color.light_blue_gray));
+                this.gameStarterUserButton.setTextColor(this.getResources().getColor(R.color.dark_blue_gray));
         }
     }
 
@@ -191,69 +133,57 @@ public class GameConfigurationActivity extends AppCompatActivity {
         button.setTextColor(this.getResources().getColor(R.color.dark_gray));
     }
 
+    @OnClick({ R.id.game_configuration_level_easy_button,
+            R.id.game_configuration_level_medium_button,
+            R.id.game_configuration_level_hard_button  })
+    public void onLevelButtonClicked(Button button) {
 
-    /* View */
-
-    private Button levelEasyButton() {
-        if(this.levelEasyButton == null)
-            this.levelEasyButton =
-                    (Button) findViewById(R.id.game_configuration_level_easy_button);
-
-        return this.levelEasyButton;
+        switch (button.getId()) {
+            case R.id.game_configuration_level_easy_button:
+                this.chooseLevel(GameLevelEnum.EASY);
+                break;
+            case R.id.game_configuration_level_medium_button:
+                this.chooseLevel(GameLevelEnum.MEDIUM);
+                break;
+            case R.id.game_configuration_level_hard_button:
+                this.chooseLevel(GameLevelEnum.HARD);
+                break;
+            default:
+        }
     }
 
-    private Button levelMediumButton() {
-        if(this.levelMediumButton == null)
-            this.levelMediumButton =
-                    (Button) findViewById(R.id.game_configuration_level_medium_button);
+    @OnClick({ R.id.game_configuration_user_symbol_x_button,
+            R.id.game_configuration_user_symbol_o_button })
+    public void onSymbolButtonClicked(Button button) {
 
-        return this.levelMediumButton;
+        switch (button.getId()) {
+            case R.id.game_configuration_user_symbol_x_button:
+                GameConfigurationActivity.this.chooseUserSymbol(SymbolEnum.X);
+                break;
+            case R.id.game_configuration_user_symbol_o_button:
+                GameConfigurationActivity.this.chooseUserSymbol(SymbolEnum.O);
+                break;
+            default:
+        }
     }
 
-    private Button levelHardButton() {
-        if(this.levelHardButton == null)
-            this.levelHardButton =
-                    (Button) findViewById(R.id.game_configuration_level_hard_button);
+    @OnClick({ R.id.game_configuration_game_starter_user_button,
+            R.id.game_configuration_game_starter_computer_button })
+    public void onGameStarterButtonClicked(Button button) {
 
-        return this.levelHardButton;
+        switch (button.getId()) {
+            case R.id.game_configuration_game_starter_user_button:
+                GameConfigurationActivity.this.chooseGameStarter(GameStarterEnum.USER);
+                break;
+            case R.id.game_configuration_game_starter_computer_button:
+                GameConfigurationActivity.this.chooseGameStarter(GameStarterEnum.COMPUTER);
+                break;
+            default:
+        }
     }
 
-    private Button symbolXButton() {
-        if(this.symbolXButton == null)
-            this.symbolXButton =
-                    (Button) findViewById(R.id.game_configuration_user_symbol_x_button);
-
-        return this.symbolXButton;
-    }
-
-    private Button symbolOButton() {
-        if(this.symbolOButton == null)
-            this.symbolOButton =
-                    (Button) findViewById(R.id.game_configuration_user_symbol_o_button);
-
-        return this.symbolOButton;
-    }
-
-    private Button gameStarterUserButton() {
-        if(this.gameStarterUserButton == null)
-            this.gameStarterUserButton =
-                    (Button) findViewById(R.id.game_configuration_game_starter_user_button);
-
-        return gameStarterUserButton;
-    }
-
-    private Button gameStarterComputerButton() {
-        if(this.gameStarterComputerButton == null)
-            this.gameStarterComputerButton =
-                    (Button) findViewById(R.id.game_configuration_game_starter_computer_button);
-
-        return gameStarterComputerButton;
-    }
-
-    private Button playButton() {
-        if(this.playButton == null)
-            this.playButton = (Button) findViewById(R.id.game_configuration_play_button);
-
-        return playButton;
+    @OnClick(R.id.game_configuration_play_button)
+    public void onPlayButtonClicked() {
+        GameActivity.startActivity(this, this.gameConfiguration);
     }
 }
